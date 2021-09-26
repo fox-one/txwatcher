020457e554d65a6329c0b6b60773db5062daa5d4
yiplee <guoyinl@gmail.com>
2021-09-27 02:09:10 +0800

    init project

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/fox-one/txwatcher
+
+go 1.17
+
+require (
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/fox-one/mixin-sdk-go v1.5.0 // indirect
+	github.com/fox-one/msgpack v1.0.0 // indirect
+	github.com/go-resty/resty/v2 v2.6.0 // indirect
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
+	golang.org/x/net v0.0.0-20210510095157-81045d8b478c // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/fox-one/mixin-sdk-go"
+)
+
+var (
+	configPath = flag.String("config","","keystore path")
+)
+
+func loadKeystore() (*mixin.Keystore,error) {
+	f,err := os.Open(*configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	var keystore mixin.Keystore
+	if err := json.NewDecoder(f).Decode(&keystore);err != nil {
+		return nil, err
+	}
+
+	return &keystore,err
+}
+
+func main() {
+	flag.Parse()
+
+	store,err := loadKeystore()
+	if err != nil {
+		log.Fatalln("loadKeystore",err)
+	}
+
+	client,err := mixin.NewFromKeystore(store)
+	if err != nil {
+		log.Fatalln("init mixin client",err)
+	}
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+	defer stop()
+
+	offset := time.Unix(0,0)
+	const limit = 500
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(time.Second):
+			outputs,err := client.ReadMultisigs(ctx,offset,limit)
+			if err != nil {
+				log.Println("ReadMultisigs",err)
+				break
+			}
+
+			for _,output := range outputs {
+				offset = output.CreatedAt
+
+				if err := handleOutput(ctx,client,output);err != nil {
+					log.Println("handleOutput",output.UTXOID,output.State,err)
+				}
+			}
+
+			if len(outputs) < limit {
+				offset = time.Unix(0,0)
+			}
+		}
+	}
+}
+
+func handleOutput(ctx context.Context,client *mixin.Client,output *mixin.MultisigUTXO) error {
+	if output.State != mixin.UTXOStateSigned {
+		return nil
+	}
+
+	sig, err := client.CreateMultisig(ctx, mixin.MultisigActionSign, output.SignedTx)
+	if err != nil {
+		return fmt.Errorf("CreateMultisig: %w",err)
+	}
+
+	if len(sig.Signers) < int(sig.Threshold) {
+		return nil
+	}
+
+	if _,err := client.SendRawTransaction(ctx,sig.RawTransaction);err != nil {
+		return fmt.Errorf("SendRawTransaction: %w",err)
+	}
+
+	return nil
+}
